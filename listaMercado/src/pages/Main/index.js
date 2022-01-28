43c98bf86ef362006e9aaeeb5f5c8bc4867bd82b
John <jhow.mito11@gmail.com>
2022-01-28 12:19:45 -0300

    commit final do app

--- listaMercado/src/pages/Main/index.js
+++ listaMercado/src/pages/Main/index.js
@@ -1,45 +1,21 @@
 import React, { useReducer, useState } from 'react';
 import { View, Image, Text, TextInput, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import styles from '../../styles/styles';
-
+import useMarketList from '../../hooks/useMarketList';
 import { sha256 } from 'react-native-sha256'
 
 
 const Main = () => {
-  const initialState = [];
 
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'ADD':
-        return [...state, action.item];
-
-      case 'CHECK':
-        return state.map(item => {
-          if (item.id === action.id) {
-            return { ...item, check: !item.check }
-          } else {
-            return item;
-          }
-        });
-
-      case 'REMOVE':
-        return state.filter (item =>{
-          return item.id !== action.id;
-        });
-      default:
-        return state;
-    }
-  };
   const [product, setProduct] = useState('')
-  const [state, dispatch] = useReducer(reducer, initialState)
-
+  const [state, addItem, checkItem, removeItem] = useMarketList()
 
 
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
-        autoCapitalize='sentences'
+          autoCapitalize='sentences'
           style={styles.input}
           placeholder='Adicionar produto'
           value={product}
@@ -51,17 +27,7 @@ const Main = () => {
 
         <TouchableOpacity style={styles.addButton}
           onPress={async () => {
-
-            const hashId = await sha256(product);
-        
-            dispatch({
-              type: 'ADD',
-              item: {
-                id: hashId,
-                title: product,
-                check: false
-              },
-            })
+            addItem(product)
             setProduct('')
           }}>
           <Text style={styles.textButton}>+</Text>
@@ -71,28 +37,22 @@ const Main = () => {
       <FlatList
         data={state}
         renderItem={({ item }) => (
-         <View style={styles.itemsContainer}>
-          <TouchableOpacity
-          style={styles.itemCheckButton}
-            onPress={() => {
-              dispatch({
-                type: 'CHECK',
-                id: item.id,
-              })
-            }}
-          >
-            <Text style={[styles.listItem, item.check ? styles.listItemChecked : '',]}>{item.title} </Text>
-          </TouchableOpacity >
-          <TouchableOpacity style={styles.removeItem}
-          onPress={() =>{
-            dispatch({
-              type: 'REMOVE',
-              id: item.id,
-            })
-          }}
-          >
-            <Text style={styles.removeItemText} >Remover</Text>
-          </TouchableOpacity>
+          <View style={styles.itemsContainer}>
+            <TouchableOpacity
+              style={styles.itemCheckButton}
+              onPress={() => {
+                checkItem(item.id)
+              }}
+            >
+              <Text style={[styles.listItem, item.check ? styles.listItemChecked : '',]}>{item.title} </Text>
+            </TouchableOpacity >
+            <TouchableOpacity style={styles.removeItem}
+              onPress={() => {
+                removeItem(item.id)
+              }}
+            >
+              <Text style={styles.removeItemText} >Remover</Text>
+            </TouchableOpacity>
           </View>
         )}
       />
